3c40114e5eaf6f81fa1056ec897d8e1fd644636c
tranvilap <48132959+tranvilap@users.noreply.github.com>
2020-04-05 00:06:35 +0700

    Added simple BFS pathfinding

--- Assets/PathFinder.cs
+++ Assets/PathFinder.cs
@@ -8,14 +8,63 @@ public class PathFinder : MonoBehaviour
     [SerializeField] Color startWaypointColor = Color.red;
     [SerializeField] Color endWaypointColor = Color.green;
     Dictionary<Vector2, Waypoint> grid = new Dictionary<Vector2, Waypoint>();
+    readonly Vector2[] directions = new Vector2[]
+    {
+        Vector2.up, Vector2.down,
+        Vector2.left, Vector2.right
+    };
     // Start is called before the first frame update
+
     void Awake()
     {
         LoadWaypoints();
+        FindPathBFS();
+
     }
+
+    private void FindPathBFS()
+    {
+        System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
+        stopwatch.Start();
+        bool endFound = false;
+        Queue<Waypoint> path = new Queue<Waypoint>();
+        HashSet<Waypoint> visited = new HashSet<Waypoint>();
+
+        path.Enqueue(startWaypoint);
+        while (path.Count > 0 && !endFound)
+        {
+            var waypoint = path.Dequeue();
+            visited.Add(waypoint);
+            foreach (var direction in directions)
+            {
+                grid.TryGetValue(waypoint.GridPos + direction, out Waypoint next);
+                if (next != null)
+                {
+                    if (next == endWaypoint)
+                    {
+                        SetWaypointTopColor(next, Color.black);
+                        endFound = true;
+                        break;
+                    }
+                    if (!visited.Contains(next))
+                    {
+                        path.Enqueue(next);
+                        SetWaypointTopColor(next, Color.yellow);
+                    }
+                }
+            }
+        }
+        stopwatch.Stop();
+        if (!endFound)
+        {
+            Debug.LogWarning("Couldn't find end waypoint");
+        }
+        Debug.LogError(stopwatch.Elapsed.TotalSeconds);
+    }
+
     private bool CheckStartEndWaypoints()
     {
-        if(startWaypoint == null)
+        if (startWaypoint == null)
         {
             Debug.LogError("Missing Start Waypoint", gameObject);
             return false;
@@ -31,7 +80,7 @@ public class PathFinder : MonoBehaviour
     {
         if (!CheckStartEndWaypoints()) { return; }
         var waypoints = FindObjectsOfType<Waypoint>();
-        Vector2 startGridPos=startWaypoint.GridPos;
+        Vector2 startGridPos = startWaypoint.GridPos;
         Vector2 endGridPos = endWaypoint.GridPos;
         foreach (var waypoint in waypoints)
         {
@@ -71,7 +120,7 @@ public class PathFinder : MonoBehaviour
             Debug.LogError("Couldn't get End Waypoint");
         }
         //Check if Start Waypoint is the same as End Waypoint
-        if(startWaypoint == endWaypoint)
+        if (startWaypoint == endWaypoint)
         {
             Debug.LogError("Start Waypoint is the same as End Waypoint.", startWaypoint);
         }
